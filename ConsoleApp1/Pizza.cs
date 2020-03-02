4bffc87c5a64791c5d4db1c650bc788bd537c4d9
Viktor Melnyk <viktormelnyk@n-ix.com>
2020-03-02 13:32:24 +0200

    Add project files.

---
+++ ConsoleApp1/Pizza.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Pizza
+    {
+        public string pizzaName;
+        public string[] extrasNames = new string[5];
+
+        public Pizza()
+        {
+            this.pizzaName = "margarita";
+        }
+
+        public Pizza(string pizzaName)
+        {
+            this.pizzaName = pizzaName;
+        }
+
+        public Pizza(string pizzaName, string[] extrasNames)
+        {
+            this.pizzaName = pizzaName;
+            for (int i = 0; i < extrasNames.Length; i++)
+            {
+                this.extrasNames[i] = extrasNames[i];
+            }
+        }
+
+        public void ShowPizzaName(string pizzaName)
+        {
+            Console.Clear();
+            Console.WriteLine("Your PIzza: \n " + pizzaName);
+            Console.ReadLine();
+        }
+
+        public void ShowPizzaWithExtras (string pizzaName, int[] extrasSelection, string[] extrasNames)
+        {
+            Console.Clear();
+            Console.WriteLine("Your pizza:\n" + pizzaName + " with additional: ");
+            for (int i = 0; i < extrasSelection.Length; i++)
+            {
+                if (extrasSelection[i] == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(extrasNames[extrasSelection[i] - 1]);
+                }
+
+            }
+            Console.ReadLine();
+
+
+        }
+
+        public int[] SelectPizzaExtras (string[] extrasNames)
+        {
+            int[] extrasSelection = new int[5];
+
+
+            return extrasSelection;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void ShowMenu(string[] inputArray, string userMessage)
+        {
+            Console.WriteLine(userMessage);
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                Console.WriteLine(i + 1 + ": " + inputArray[i]);
+            };
+        }
+
+        static void ShowConfirmation(int selectedPizza, int[] selectedExtras, string[] allPizzas, string[] allExtras)
+        {
+           
+        }
+
+        static void main(string[] args)
+        {
+            //defining menu, user choices, counters
+            string[] pizzalist = { "margarita", "cesario", "bavarian" };
+            string[] extras = { "cheese", "cream sauce", "mushrooms", "chicken" };
+            int[] extrasfinalchoice = new int[4];
+            int choice;
+            int extraschoice = -1;
+            int choicecounter = 0;
+            string pizzamessage = "select your pizza:";
+            string extrasmessage = "select your extras (optional up to 4 ingredients). press '0' to skip this step:";
+
+
+            // get user choice on pizza
+            ShowMenu(pizzalist, pizzamessage);
+            choice = int.Parse(Console.ReadLine()) - 1;
+            Console.Clear();
+
+            // get user choice on extras:
+            while (extraschoice != 0 && choicecounter < extrasfinalchoice.Length)
+            {
+                Console.Clear();
+                ShowMenu(extras, extrasmessage);
+
+                extraschoice = int.Parse(Console.ReadLine()); ///remove extras choisej
+                extrasfinalchoice[choicecounter] = extraschoice;
+                choicecounter += 1;
+            };
+
+
+            // show confirmation of user choice
+            ShowConfirmation(choice, extrasfinalchoice, pizzalist, extras);
+
+            Console.ReadKey();
+        }
+    }
+}
